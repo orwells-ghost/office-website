945f282943e910ed5a5525ad54e027a8c76bf71b
Ryan Kennedy <ryan+github@ryankennedy.me>
2018-11-03 13:48:47 -0700

    Complete

---
+++ js/app.js
@@ -0,0 +1,69 @@
+///////////
+// Elements
+///////////
+const elements = {
+  bannerNums: Array.from(document.querySelectorAll('.banner__stat--figure')),
+  hamburger: document.querySelector('.header__hamburger'),
+  hamburgerChildren: Array.from(document.querySelector('.header__hamburger').children),
+  headerLinkList: document.querySelector('.header__links'),
+  headerLinks: Array.from(document.querySelectorAll('.header__link')),
+  tSlides: Array.from(document.querySelectorAll('.testimonial__text-box')),
+  tBtns: Array.from(document.querySelectorAll('.testimonial__dot'))
+}
+if (document.body.classList.contains('homepage')) {
+  const incNums = el => {
+    let upperLimit = parseInt(el.innerHTML, 10), lowerLimit = 0;
+    el.textConent = '0';
+  
+    const inv = setInterval(function() {     
+        if(lowerLimit < upperLimit)
+            el.innerHTML = ++lowerLimit;
+        else
+            clearInterval(inv);
+    }, 3000 / 300);
+  };
+
+  // Testimonial Slider
+  elements.tBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+      event.preventDefault();
+      fadeTestimonials(event.target.getAttribute('href'));
+    })
+  })
+
+  const fadeTestimonials = num => {
+    let inn, out;
+    num == 1 ? (inn = 1, out = 2) : (inn = 2, out = 1);
+    const elToFadeIn = document.querySelector(`.testimonial${inn}`), elToFadeOut = document.querySelector(`.testimonial${out}`);
+    setTimeout(function(){
+      elToFadeOut.classList.remove('is-visible');
+      console.log(`Fading out ${elToFadeOut}`)
+
+      setTimeout(function(){
+        elToFadeIn.classList.add('is-visible');
+      }, 450);
+    }, 350);
+  }
+}
+
+elements.bannerNums.forEach(el => incNums(el));
+document.querySelector('.header__hamburger').addEventListener('click', event => {
+  elements.headerLinkList.classList.toggle('header__links--mobile');
+  elements.headerLinks.forEach(el => el.classList.toggle('header__link--mobile'));
+});
+
+// Detect click outside of mobile nav, remove relevant classes
+window.addEventListener('click', event => {
+  if (!elements.headerLinks.includes(event.target) && event.target != elements.headerLinkList && event.target != elements.hamburger && !elements.hamburgerChildren.includes(event.target)) {
+    elements.headerLinkList.classList.remove('header__links--mobile');
+    elements.headerLinks.forEach(el => el.classList.remove('header__link--mobile'));
+  }
+})
+
+// Check if window width changed
+window.addEventListener('resize', event => {
+  if (window.innerWidth > 1000) {
+    elements.headerLinkList.classList.remove('header__links--mobile');
+    elements.headerLinks.forEach(el => el.classList.remove('header__link--mobile'));
+  }
+})
---
+++ js/map.js
@@ -0,0 +1,191 @@
+///////////
+// Google Maps
+///////////
+var map;
+function initMap() {
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.706110785437545, lng: -73.94261113374876},
+      zoom: 12,
+      styles: [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#263c3f"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#6b9a76"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#38414e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#212a37"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9ca5b3"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#1f2835"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#f3d19c"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#2f3948"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#515c6d"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        }
+      ]      
+    });
+    var myLatLng1 = {lat: 40.716110785437545, lng: -73.99961113374876};
+    var myLatLng2 = {lat: 40.686110785437545, lng: -73.93951113374876};
+    var myLatLng3 = {lat: 40.731110785437545, lng: -73.79961113374876};
+    
+    var marker1 = new google.maps.Marker({
+        position: myLatLng1,
+        map: map,
+        title: 'NY Office'
+    });
+    var marker2 = new google.maps.Marker({
+      position: myLatLng2,
+      map: map,
+      title: 'Brooklyn Office'
+    });
+    var marker3 = new google.maps.Marker({
+      position: myLatLng3,
+      map: map,
+      title: 'Queens Office'
+    });
+}
